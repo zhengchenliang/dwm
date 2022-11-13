9e6e01782ef50bca5da7f2540a660d0ff80dd7cc
zhengchenliang <talie.firzen@foxmail.com>
2022-11-14 03:39:30 +0800

    keyboard settings

--- config.h
+++ config.h
@@ -7,8 +7,8 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor */
+static const int showsystray        = 1;        /* 0 means no systray */
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
@@ -24,8 +24,8 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#ffffff";
 static const char col_cyan[]        = "#37474F";
-static const char col_border[]        = "#42A5F5";
-static const unsigned int baralpha = 0xd0;
+static const char col_border[]      = "#42A5F5";
+static const unsigned int baralpha  = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
@@ -40,36 +40,35 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-//static const char *tags[] = { "一", "二", "三", "四", "五", "六", "七", "八", "九" };
-static const char *tags[] = { "\uf120", "\uf7ae", "\uf121", "\uf04b", "\ue62e", "\uf251", "\ue727", "\uf537", "\uf684" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Android Emulator", NULL,       NULL,       0,            1,           -1 },
-	{ "Emulator", NULL,       NULL,       0,            1,           -1 },
+	/* class               instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",              NULL,       NULL,       0,            1,           -1 },
+	{ "Android Emulator",  NULL,       NULL,       0,            1,           -1 },
+	{ "Emulator",          NULL,       NULL,       0,            1,           -1 },
 	{ "quemu-system-i386", NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Firefox",           NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
 static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int nmaster     = 1;   /* number of clients in master area */
+static const int resizehints = 1;   /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "Tile",      tile },    /* first entry is default */
+	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
 
 /* key definitions */
-#define MODKEY Mod4Mask
+#define MODKEY Mod1Mask // VITAL control key = alt
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -79,91 +78,93 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
-/* commands */
+/* commands */ // VITAL launch scripts
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+
 static const char *browsercmd[]  = { "google-chrome-stable", NULL };
+
+static const char *screenshotcmd[] = { "flameshot", "gui", NULL };
+
+static const char *setqwertycmd[]  = { "/home/david/scripts/setxmodmap-qwerty.sh", NULL };
+static const char *setcolemakcmd[]  = { "/home/david/scripts/setxmodmap-colemak.sh", NULL };
+static const char *suspendcmd[]  = { "/home/david/scripts/suspend.sh", NULL };
+static const char *sktogglecmd[]  = { "/home/david/scripts/sck-tog.sh", NULL };
+static const char *wpcmd[]  = { "/home/david/scripts/wp-change.sh", NULL };
 
 static const char *upvol[]   = { "/home/david/scripts/vol-up.sh",  NULL };
 static const char *downvol[] = { "/home/david/scripts/vol-down.sh",  NULL };
 static const char *mutevol[] = { "/home/david/scripts/vol-toggle.sh",  NULL };
 
-static const char *wpcmd[]  = { "/home/david/scripts/wp-change.sh", NULL };
-static const char *sktogglecmd[]  = { "/home/david/scripts/sck-tog.sh", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "80x24", NULL };
 
-static const char *setcolemakcmd[]  = { "/home/david/scripts/setxmodmap-colemak.sh", NULL };
-static const char *setqwertycmd[]  = { "/home/david/scripts/setxmodmap-qwerty.sh", NULL };
 
-static const char *suspendcmd[]  = { "/home/david/scripts/suspend.sh", NULL };
-
-static const char *screenshotcmd[] = { "flameshot", "gui", NULL };
-
-static Key keys[] = {
+static Key keys[] = { // VITAL key complex
 	/* modifier            key                      function        argument */
-	{ MODKEY,              XK_s,                    spawn,          {.v = dmenucmd } },
-	{ MODKEY,              XK_Return,               spawn,          {.v = termcmd } },
-	{ MODKEY,              XK_c,                    spawn,          {.v = browsercmd } },
-	{ MODKEY|ShiftMask,    XK_w,                    spawn,          {.v = setqwertycmd } },
-	{ MODKEY|ShiftMask,    XK_m,                    spawn,          {.v = setcolemakcmd } },
-	{ MODKEY|ShiftMask,    XK_p,                    spawn,          {.v = suspendcmd } },
-	{ MODKEY|ControlMask,  XK_s,                    spawn,          {.v = sktogglecmd } },
-	{ 0,                   XF86XK_AudioLowerVolume, spawn,          {.v = downvol } },
-	{ 0,                   XF86XK_AudioMute,        spawn,          {.v = mutevol } },
-	{ 0,                   XF86XK_AudioRaiseVolume, spawn,          {.v = upvol   } },
-	{ MODKEY,              XK_bracketleft,          spawn,          {.v = downvol } },
-	{ MODKEY,              XK_backslash,            spawn,          {.v = mutevol } },
-	{ MODKEY,              XK_bracketright,         spawn,          {.v = upvol   } },
-	{ MODKEY,              XK_b,                    spawn,          {.v = wpcmd } },
-	{ 0,                   XK_Print,                spawn,          {.v = screenshotcmd } },
-	{ MODKEY|ShiftMask,    XK_e,                    rotatestack,    {.i = +1 } },
-	{ MODKEY|ShiftMask,    XK_u,                    rotatestack,    {.i = -1 } },
-	{ MODKEY,              XK_e,                    focusstack,     {.i = +1 } },
-	{ MODKEY,              XK_u,                    focusstack,     {.i = -1 } },
-	{ MODKEY,              XK_n,                    viewtoleft,     {0} },
-	{ MODKEY,              XK_i,                    viewtoright,    {0} },
-	{ MODKEY|ShiftMask,    XK_n,                    tagtoleft,      {0} },
-	{ MODKEY|ShiftMask,    XK_i,                    tagtoright,     {0} },
-	{ MODKEY|ShiftMask,    XK_h,                    incnmaster,     {.i = +1 } },
-	{ MODKEY|ShiftMask,    XK_l,                    incnmaster,     {.i = -1 } },
-	{ MODKEY,              XK_h,                    setmfact,       {.f = -0.05} },
-	{ MODKEY,              XK_l,                    setmfact,       {.f = +0.05} },
-	{ MODKEY,              XK_k,                    hidewin,        {0} },
-	{ MODKEY|ShiftMask,    XK_k,                    restorewin,     {0} },
-	{ MODKEY,              XK_o,                    hideotherwins,  {0}},
-	{ MODKEY|ShiftMask,    XK_o,                    restoreotherwins, {0}},
-	{ MODKEY|ShiftMask,    XK_Return,               zoom,           {0} },
-	{ MODKEY,              XK_Tab,                  view,           {0} },
-	{ MODKEY|ShiftMask,    XK_q,                    killclient,     {0} },
-	{ MODKEY,              XK_t,                    setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,              XK_m,                    setlayout,      {.v = &layouts[2]} },
-	{ MODKEY|ShiftMask,    XK_f,                    fullscreen,     {0} },
-	{ MODKEY,              XK_space,                setlayout,      {0} },
-	{ MODKEY|ShiftMask,    XK_space,                togglefloating, {0} },
-	{ MODKEY,              XK_apostrophe,           togglescratch,  {.v = scratchpadcmd } },
-	{ MODKEY,              XK_0,                    view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,    XK_0,                    tag,            {.ui = ~0 } },
-	{ MODKEY,              XK_comma,                focusmon,       {.i = -1 } },
-	{ MODKEY,              XK_period,               focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,    XK_comma,                tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,    XK_period,               tagmon,         {.i = +1 } },
-	TAGKEYS(               XK_1,                      0)
-	TAGKEYS(               XK_2,                      1)
-	TAGKEYS(               XK_3,                      2)
-	TAGKEYS(               XK_4,                      3)
-	TAGKEYS(               XK_5,                      4)
-	TAGKEYS(               XK_6,                      5)
-	TAGKEYS(               XK_7,                      6)
-	TAGKEYS(               XK_8,                      7)
-	TAGKEYS(               XK_9,                      8)
-	{ MODKEY|ControlMask,  XK_q,      quit,           {0} },
+	{ MODKEY,              XK_a,                    spawn,          {.v = dmenucmd } }, // open top dialogue - dmenu(1)
+	{ MODKEY,              XK_s,                    spawn,          {.v = termcmd } }, // open new terminal - st(1)
+	{ MODKEY,              XK_b,                    spawn,          {.v = browsercmd } }, // open browser - defined above google-chrome-stable
+	{ 0,                   XK_Print,                spawn,          {.v = screenshotcmd } }, // open screenshot - defined above flameshot
+	{ MODKEY|ShiftMask,    XK_z,                    spawn,          {.v = setqwertycmd } }, // open keyset - defined above sh
+	{ MODKEY|ShiftMask,    XK_x,                    spawn,          {.v = setcolemakcmd } }, // open colemak - defined above sh
+	{ MODKEY|ShiftMask,    XK_c,                    spawn,          {.v = suspendcmd } }, // open suspend - defined above sh
+	{ MODKEY|ShiftMask,    XK_v,                    spawn,          {.v = sktogglecmd } }, // open scktog - defined above sh
+	{ MODKEY|ShiftMask,    XK_b,                    spawn,          {.v = wpcmd } }, // open wpchange - defined above sh
+	{ 0,                   XF86XK_AudioLowerVolume, spawn,          {.v = downvol } }, // volume ctrl - defined above sh
+	{ 0,                   XF86XK_AudioMute,        spawn,          {.v = mutevol } }, // volume ctrl - defined above sh
+	{ 0,                   XF86XK_AudioRaiseVolume, spawn,          {.v = upvol   } }, // volume ctrl - defined above sh
+	{ MODKEY,              XK_bracketleft,          spawn,          {.v = downvol } }, // volume ctrl - defined above sh
+	{ MODKEY,              XK_backslash,            spawn,          {.v = mutevol } }, // volume ctrl - defined above sh
+	{ MODKEY,              XK_bracketright,         spawn,          {.v = upvol   } }, // volume ctrl - defined above sh
+	{ MODKEY|ShiftMask,    XK_l,                    rotatestack,    {.i = +1 } }, // rotate window to next window
+	{ MODKEY|ShiftMask,    XK_j,                    rotatestack,    {.i = -1 } }, // rotate window to previous window
+	{ MODKEY,              XK_l,                    focusstack,     {.i = +1 } }, // focus window to next window
+	{ MODKEY,              XK_j,                    focusstack,     {.i = -1 } }, // focus window to previous window
+	{ MODKEY|ControlMask,  XK_n,                    viewtoleft,     {0} }, // 
+	{ MODKEY|ControlMask,  XK_m,                    viewtoright,    {0} }, //
+	{ MODKEY|ShiftMask,    XK_n,                    tagtoleft,      {0} }, //
+	{ MODKEY|ShiftMask,    XK_m,                    tagtoright,     {0} }, //
+	{ MODKEY,              XK_o,                    incnmaster,     {.i = +1 } }, // increase one more main window (vertical layout)
+	{ MODKEY,              XK_p,                    incnmaster,     {.i = -1 } }, // decrease one more main window (vertical layout)// windows in page - main window in page left per vertical block - focus window is highlighted - windows can be tiled or floating
+	{ MODKEY,              XK_k,                    setmfact,       {.f = -0.05} }, // make main window smaller
+	{ MODKEY,              XK_i,                    setmfact,       {.f = +0.05} }, // make main window bigger
+	{ MODKEY,              XK_u,                    hidewin,        {0} }, // hide focus window
+	{ MODKEY|ShiftMask,    XK_u,                    restorewin,     {0} }, // show hided window
+	{ MODKEY,              XK_y,                    hideotherwins,  {0} }, // hide other windows
+	{ MODKEY|ShiftMask,    XK_y,                    restoreotherwins,{0}}, // show other windows
+	{ MODKEY,              XK_f,                    zoom,           {0} }, // throw/bounce focus window to main window
+	{ MODKEY,              XK_g,                    view,           {0} }, // switch between current page and the previous page
+	{ MODKEY,              XK_t,                    killclient,     {0} }, // terminate focus window
+	{ MODKEY,              XK_q,                    setlayout,      {.v = &layouts[0]} }, // set layout mode - tiled - fixed window
+	{ MODKEY,              XK_w,                    setlayout,      {.v = &layouts[1]} }, // set layout mode - float - floating window
+	{ MODKEY,              XK_e,                    setlayout,      {.v = &layouts[2]} }, // set layout mode - monocle - focus window fullscreen
+	{ MODKEY,              XK_r,                    setlayout,      {0} }, // switch between current layout mode and previous one
+	{ MODKEY|ShiftMask,    XK_r,                    fullscreen,     {0} }, // window fullscreen
+	{ MODKEY,              XK_space,                togglefloating, {0} }, // switch focus window between tiled/floating modes
+	{ MODKEY,              XK_apostrophe,           togglescratch,  {.v = scratchpadcmd } }, // scratchpad defined above
+	{ MODKEY,              XK_0,                    view,           {.ui = ~0 } }, // view all windows linked to any page
+	{ MODKEY|ShiftMask,    XK_0,                    tag,            {.ui = ~0 } }, // link focus window to all pages
+	{ MODKEY|ControlMask,  XK_comma,                focusmon,       {.i = -1 } }, // focus on previous page
+	{ MODKEY|ControlMask,  XK_period,               focusmon,       {.i = +1 } }, // focus on next page
+	{ MODKEY|ShiftMask,    XK_comma,                tagmon,         {.i = -1 } }, // send focus window to previous page
+	{ MODKEY|ShiftMask,    XK_period,               tagmon,         {.i = +1 } }, // send focus window to next page
+	TAGKEYS(               XK_1,                                    0) // ModMask + 5 = switch current page to page 5
+	TAGKEYS(               XK_2,                                    1) // ModMask + Shift + 5 = link focus window to page 5
+	TAGKEYS(               XK_3,                                    2) // ModMask + Shift + Ctrl + 5 = link/unlink focus window to/from page 5
+	TAGKEYS(               XK_4,                                    3) // ModeMask + Ctrl + 5 = bring/bonce all windows linked to page 5 to current page
+	TAGKEYS(               XK_5,                                    4)
+	TAGKEYS(               XK_6,                                    5)
+	TAGKEYS(               XK_7,                                    6)
+	TAGKEYS(               XK_8,                                    7)
+	TAGKEYS(               XK_9,                                    8)
+	{ MODKEY|ShiftMask,    XK_p,                    quit,           {0} }, // exit dwm
 };
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
-static Button buttons[] = {
+static Button buttons[] = { // VITAL mouse simplex
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
