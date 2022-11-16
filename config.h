e571bf9449f67e1aa48588ced9920d168a9c84c4
zhengchenliang <talie.firzen@foxmail.com>
2022-11-17 03:03:20 +0800

    my standard keyboard

--- config.h
+++ config.h
@@ -92,7 +92,7 @@ static char dmenumon[2] = "0"; // VITAL dmenu options colors applied here
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_white, "-sb", col_cyan, "-sf", col_black, "-b", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
-static const char *browsercmd[]  = { "google-chrome-stable", NULL };
+static const char *browsercmd[]  = { "firefox", NULL };
 
 static const char *screenshotcmd[] = { "flameshot", "gui", NULL };
 
@@ -109,56 +109,64 @@ static const char *mutevol[] = { "~/D0_desktop_base/dwm_me/scripts/david/scripts
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "80x24", NULL };
 
-
 static Key keys[] = { // VITAL key complex
 	/* modifier            key                      function        argument */
+	{ MODKEY,              XK_q,                    setlayout,      {.v = &layouts[0]} }, // set layout mode - tiled - fixed window
+	{ MODKEY,              XK_w,                    setlayout,      {.v = &layouts[1]} }, // set layout mode - float - floating window
+	{ MODKEY,              XK_e,                    setlayout,      {.v = &layouts[2]} }, // set layout mode - monocle - focus window fullscreen
+	{ MODKEY,              XK_r,                    setlayout,      {0} }, // switch between current layout mode and previous one
+	{ MODKEY|ShiftMask,    XK_r,                    fullscreen,     {0} }, // window fullscreen
+
 	{ MODKEY,              XK_a,                    spawn,          {.v = dmenucmd } }, // open top dialogue - dmenu(1)
 	{ MODKEY,              XK_s,                    spawn,          {.v = termcmd } }, // open new terminal - st(1)
-	{ MODKEY,              XK_b,                    spawn,          {.v = browsercmd } }, // open browser - defined above google-chrome-stable
-	{ 0,                   XK_Print,                spawn,          {.v = screenshotcmd } }, // open screenshot - defined above flameshot
+	{ MODKEY,              XK_d,                    killclient,     {0} }, // terminate focus window
+	{ MODKEY,              XK_f,                    zoom,           {0} }, // throw/bounce focus window to main window
+	{ MODKEY,              XK_g,                    view,           {0} }, // switch between current page and the previous page
+
 	{ MODKEY|ShiftMask,    XK_z,                    spawn,          {.v = setqwertycmd } }, // open keyset - defined above sh
 	{ MODKEY|ShiftMask,    XK_x,                    spawn,          {.v = setcolemakcmd } }, // open colemak - defined above sh
 	{ MODKEY|ShiftMask,    XK_c,                    spawn,          {.v = suspendcmd } }, // open suspend - defined above sh
 	{ MODKEY|ShiftMask,    XK_v,                    spawn,          {.v = sktogglecmd } }, // open scktog - defined above sh
 	{ MODKEY|ShiftMask,    XK_b,                    spawn,          {.v = wpcmd } }, // open wpchange - defined above sh
+	{ MODKEY,              XK_b,                    spawn,          {.v = browsercmd } }, // open browser - defined above google-chrome-stable
+
+	{ MODKEY,              XK_i,                    setmfact,       {.f = +0.05} }, // make main window bigger
+	{ MODKEY,              XK_k,                    setmfact,       {.f = -0.05} }, // make main window smaller
+	{ MODKEY|ShiftMask,    XK_l,                    rotatestack,    {.i = +1 } }, // rotate window to next window
+	{ MODKEY|ShiftMask,    XK_j,                    rotatestack,    {.i = -1 } }, // rotate window to previous window
+	{ MODKEY,              XK_l,                    focusstack,     {.i = +1 } }, // focus window to next window
+	{ MODKEY,              XK_j,                    focusstack,     {.i = -1 } }, // focus window to previous window
+
+	{ MODKEY,              XK_o,                    incnmaster,     {.i = +1 } }, // increase one more main window (vertical layout)
+	{ MODKEY,              XK_p,                    incnmaster,     {.i = -1 } }, // decrease one more main window (vertical layout)// windows in page - main window in page left per vertical block - focus window is highlighted - windows can be tiled or floating
+
+	{ MODKEY,              XK_u,                    hidewin,        {0} }, // hide focus window
+	{ MODKEY|ShiftMask,    XK_u,                    restorewin,     {0} }, // show hided window
+	{ MODKEY,              XK_y,                    hideotherwins,  {0} }, // hide other windows
+	{ MODKEY|ShiftMask,    XK_y,                    restoreotherwins,{0}}, // show other windows
+
+	// { MODKEY|ControlMask,  XK_n,                    viewtoleft,     {0} }, // functions here are not in use 
+	// { MODKEY|ControlMask,  XK_m,                    viewtoright,    {0} }, // can add your function in dwm.c
+	// { MODKEY|ShiftMask,    XK_n,                    tagtoleft,      {0} }, // leaving the n and m keys to st
+	// { MODKEY|ShiftMask,    XK_m,                    tagtoright,     {0} }, // as the , and . keys below too
+
+	// { MODKEY|ControlMask,  XK_comma,                focusmon,       {.i = -1 } }, // focus on previous page
+	// { MODKEY|ControlMask,  XK_period,               focusmon,       {.i = +1 } }, // focus on next page
+	// { MODKEY|ShiftMask,    XK_comma,                tagmon,         {.i = -1 } }, // send focus window to previous page
+	// { MODKEY|ShiftMask,    XK_period,               tagmon,         {.i = +1 } }, // send focus window to next page
+
+	{ 0,                   XK_Print,                spawn,          {.v = screenshotcmd } }, // open screenshot - defined above flameshot
 	{ 0,                   XF86XK_AudioLowerVolume, spawn,          {.v = downvol } }, // volume ctrl - defined above sh
 	{ 0,                   XF86XK_AudioMute,        spawn,          {.v = mutevol } }, // volume ctrl - defined above sh
 	{ 0,                   XF86XK_AudioRaiseVolume, spawn,          {.v = upvol   } }, // volume ctrl - defined above sh
 	{ MODKEY,              XK_bracketleft,          spawn,          {.v = downvol } }, // volume ctrl - defined above sh
 	{ MODKEY,              XK_backslash,            spawn,          {.v = mutevol } }, // volume ctrl - defined above sh
 	{ MODKEY,              XK_bracketright,         spawn,          {.v = upvol   } }, // volume ctrl - defined above sh
-	{ MODKEY|ShiftMask,    XK_l,                    rotatestack,    {.i = +1 } }, // rotate window to next window
-	{ MODKEY|ShiftMask,    XK_j,                    rotatestack,    {.i = -1 } }, // rotate window to previous window
-	{ MODKEY,              XK_l,                    focusstack,     {.i = +1 } }, // focus window to next window
-	{ MODKEY,              XK_j,                    focusstack,     {.i = -1 } }, // focus window to previous window
-	// { MODKEY|ControlMask,  XK_n,                    viewtoleft,     {0} }, // functions here are not in use 
-	// { MODKEY|ControlMask,  XK_m,                    viewtoright,    {0} }, // can add your function in dwm.c
-	// { MODKEY|ShiftMask,    XK_n,                    tagtoleft,      {0} }, // leaving the n and m keys to st
-	// { MODKEY|ShiftMask,    XK_m,                    tagtoright,     {0} }, // as the , and . keys below too
-	{ MODKEY,              XK_o,                    incnmaster,     {.i = +1 } }, // increase one more main window (vertical layout)
-	{ MODKEY,              XK_p,                    incnmaster,     {.i = -1 } }, // decrease one more main window (vertical layout)// windows in page - main window in page left per vertical block - focus window is highlighted - windows can be tiled or floating
-	{ MODKEY,              XK_k,                    setmfact,       {.f = -0.05} }, // make main window smaller
-	{ MODKEY,              XK_i,                    setmfact,       {.f = +0.05} }, // make main window bigger
-	{ MODKEY,              XK_u,                    hidewin,        {0} }, // hide focus window
-	{ MODKEY|ShiftMask,    XK_u,                    restorewin,     {0} }, // show hided window
-	{ MODKEY,              XK_y,                    hideotherwins,  {0} }, // hide other windows
-	{ MODKEY|ShiftMask,    XK_y,                    restoreotherwins,{0}}, // show other windows
-	{ MODKEY,              XK_f,                    zoom,           {0} }, // throw/bounce focus window to main window
-	{ MODKEY,              XK_g,                    view,           {0} }, // switch between current page and the previous page
-	{ MODKEY,              XK_t,                    killclient,     {0} }, // terminate focus window
-	{ MODKEY,              XK_q,                    setlayout,      {.v = &layouts[0]} }, // set layout mode - tiled - fixed window
-	{ MODKEY,              XK_w,                    setlayout,      {.v = &layouts[1]} }, // set layout mode - float - floating window
-	{ MODKEY,              XK_e,                    setlayout,      {.v = &layouts[2]} }, // set layout mode - monocle - focus window fullscreen
-	{ MODKEY,              XK_r,                    setlayout,      {0} }, // switch between current layout mode and previous one
-	{ MODKEY|ShiftMask,    XK_r,                    fullscreen,     {0} }, // window fullscreen
 	{ MODKEY,              XK_space,                togglefloating, {0} }, // switch focus window between tiled/floating modes
 	{ MODKEY,              XK_apostrophe,           togglescratch,  {.v = scratchpadcmd } }, // scratchpad defined above
+
 	{ MODKEY,              XK_0,                    view,           {.ui = ~0 } }, // view all windows linked to any page
 	{ MODKEY|ShiftMask,    XK_0,                    tag,            {.ui = ~0 } }, // link focus window to all pages
-	// { MODKEY|ControlMask,  XK_comma,                focusmon,       {.i = -1 } }, // focus on previous page
-	// { MODKEY|ControlMask,  XK_period,               focusmon,       {.i = +1 } }, // focus on next page
-	// { MODKEY|ShiftMask,    XK_comma,                tagmon,         {.i = -1 } }, // send focus window to previous page
-	// { MODKEY|ShiftMask,    XK_period,               tagmon,         {.i = +1 } }, // send focus window to next page
 	TAGKEYS(               XK_1,                                    0) // ModMask + 5 = switch current page to page 5
 	TAGKEYS(               XK_2,                                    1) // ModMask + Shift + 5 = link focus window to page 5
 	TAGKEYS(               XK_3,                                    2) // ModMask + Shift + Ctrl + 5 = link/unlink focus window to/from page 5
@@ -168,6 +176,7 @@ static Key keys[] = { // VITAL key complex
 	TAGKEYS(               XK_7,                                    6)
 	TAGKEYS(               XK_8,                                    7)
 	TAGKEYS(               XK_9,                                    8)
+
 	{ MODKEY|ShiftMask,    XK_p,                    spawn,          SHCMD("killall .base-dwm-bar dwm") }, // exit dwm
 };
 
