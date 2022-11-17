c53061416fd358b9fb2e50537a31dfdcd50c9f13
zhengchenliang <talie.firzen@foxmail.com>
2022-11-17 09:21:00 +0800

    picom configured

--- scripts/base-dwm.sh
+++ scripts/base-dwm.sh
@@ -12,7 +12,7 @@ xset r rate 300 45 &
 setxkbmap -option caps:swapescape
 
 # First run picom --diagnostics, if glx backend fails because of fuking nvidia card then change a backend
-# picom --backend xrender &
+picom --backend xrender &
 
 source ~/.base-dwm-bar &
 # while type dwm >/dev/null; do dwm && continue || break; done
